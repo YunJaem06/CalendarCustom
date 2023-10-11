80c3b2659de316826de7f76f9f50506ebb60daa5
yunjae <yunjae0616@gmail.com>
2023-10-11 13:55:02 +0900

    첫번째 한줄 캘린더 커스텀

--- app/src/main/java/hs/project/calendarcustom/CalendarAdapter.kt
+++ app/src/main/java/hs/project/calendarcustom/CalendarAdapter.kt
@@ -1,4 +1,93 @@
 package hs.project.calendarcustom
 
-class CalendarAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class CalendarAdapter(private val listener : (calendarDateModel : CalendarDateModel, position : Int) -> Unit) :
+RecyclerView.Adapter<CalendarAdapter.MyViewHolder>(){
+
+    private val list = ArrayList<CalendarDateModel>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CalendarAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_calendar_date, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CalendarAdapter.MyViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(calendarDateModel: CalendarDateModel) {
+            val calendarDay = itemView.findViewById<TextView>(R.id.tv_calendar_day)
+            val calendarDate = itemView.findViewById<TextView>(R.id.tv_calendar_date)
+            val cardView = itemView.findViewById<CardView>(R.id.card_calendar)
+
+
+            if (calendarDateModel.isSelected) {
+                calendarDay.setTextColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.white
+                    )
+                )
+                calendarDate.setTextColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.white
+                    )
+                )
+                cardView.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.colorPrimary
+                    )
+                )
+            } else {
+                calendarDay.setTextColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.black
+                    )
+                )
+                calendarDate.setTextColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.black
+                    )
+                )
+                cardView.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.white
+                    )
+                )
+            }
+
+            calendarDay.text = calendarDateModel.calendarDay
+            calendarDate.text = calendarDateModel.calendarDate
+            cardView.setOnClickListener {
+                listener.invoke(calendarDateModel, adapterPosition)
+            }
+        }
+    }
+
+    fun setData(calendarList : ArrayList<CalendarDateModel>) {
+        list.clear()
+        list.addAll(calendarList)
+        notifyDataSetChanged()
+    }
 }
---
+++ app/src/main/java/hs/project/calendarcustom/HorizontalItemDecoration.kt
@@ -0,0 +1,25 @@
+package hs.project.calendarcustom
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class HorizontalItemDecoration(private val space : Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+
+        if (position != 0)
+            outRect.left = space
+
+        outRect.right = space
+        outRect.bottom = space
+        outRect.top = space
+
+    }
+}
